5a692fe55a0cbf20f43f2602a223a24cd8c5a9ad
ariful544 <khidirerbari@gmail.com>
2025-10-05 06:33:38 +0300

    first commit

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose,{model,models,Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        _id: {type: String, required: true},
+        name: {type: String, required: true},
+        email: {type: String, required: true,unique: true},
+        imageUrl: {type: String, required: true},
+        cartItems: {type: Object, default: {}}
+    },
+    {
+        minimize: false
+    }
+)
+
+const User = models?.user || model("user",userSchema)
+
+export default User;
